92a87e8628586c520752d332ee7badc587bbea54
guysbryant <guy.s.bryant@gmail.com>
2019-05-28 16:57:29 -0400

    Render list show page with dynamic name

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,4 +5,8 @@ class ListsController < ApplicationController
         # raise @lists.inspect #was the controller able to get the lists from the db
         #implicit render of views/lists/index.html.erb
     end
+
+    def show 
+        @list = List.find(params[:id])
+    end
 end
