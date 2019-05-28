f94b1f9a62fb85657499fc394b49a56c103d9525
guysbryant <guy.s.bryant@gmail.com>
2019-05-28 16:11:55 -0400

    Dynamically create li for every list in db

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,8 @@
 class ListsController < ApplicationController
+
+    def index 
+        @lists = List.all
+        # raise @lists.inspect #was the controller able to get the lists from the db
+        #implicit render of views/lists/index.html.erb
+    end
 end
